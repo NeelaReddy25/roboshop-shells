cad037d9b16b01a99512ef95e9b4c99573b798b7
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-23 00:55:14 +0530

    good effects

---
+++ db-tier/03-mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+dnf module disable mysql -y &>>$LOGFILE
+VALIDATE $? "disabling mysql"
+
+cp /home/centos/roboshop-shell/db-tier/mysql.repo /etc/yum.repos.d/mysql.repo &>>$LOGFILE
+VALIDATE $? "Setup MySQL repo"
+
+dnf install mysql-community-server -y &>>$LOGFILE
+VALIDATE $? "Installing mysql community server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling mysql server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting mysql server"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+# Below code will be useful for idempotent nature
+mysql -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MYSQL Root password setup"
+else
+    echo -e "MYSQL Root password is already setup...$Y SKIPPING $N"
+fi    
