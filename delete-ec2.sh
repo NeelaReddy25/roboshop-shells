cf65edf21651508a888ce09a472bf6af80e9b878
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-25 19:10:05 +0530

    good effects

--- delete-ec2.sh
+++ delete-ec2.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 source ./create-ec2.sh
-instances=("mongodb" "redis" "web")
 
-for name ${instances[@]}; do 
-    echo "Creating instance for: $name
-    aws ec2 terminate-instances --instance-ids $instance_id
-done  
+aws ec2 terminate-instances --instance-ids $instance_id
+
 
